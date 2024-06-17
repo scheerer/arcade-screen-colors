903fab014d77b44ca9c8c8e00d9da92d15e6eb3f
Russell Scheerer <russell.scheerer@gmail.com>
2024-06-17 14:33:20 -0400

    Include brightness in docs/logs, adjust defaults

--- StartScreenColors.bat
+++ StartScreenColors.bat
@@ -1,14 +1,20 @@
 # Valid values are: [AVERAGE, SQUARED_AVERAGE, MEDIAN, MODE]
 set COLOR_ALGO=AVERAGE
 
-# How often to capture the screen in milliseconds (should be greater than 50ms)
-set CAPTURE_INTERVAL=100ms
+# How often to capture the screen (should generally be greater than 50ms due to screen capture latency)
+set CAPTURE_INTERVAL=80ms
 
 # Valid values are: [LIFX]
 set LIGHT_TYPE=LIFX
 
 # Name of LIFX group to control
 set LIGHT_GROUP_NAME=ARCADE
+
+# Adjust maximum brightness of the lights between 0 and 1. 1 is full brightness. (makes screen flashes or white screens quite bright)
+set MAX_BRIGHTNESS=0.65
+
+# Adjust minimum brightness of the lights between 0 and 1. 0 is the light turned off.
+set MIN_BRIGHTNESS=0
 
 # Adjust PIXEL_GRID_SIZE to increase performance or accuracy. Lower values are slower but more accurate. 1 being the most accurate.
 set PIXEL_GRID_SIZE=5
--- cmd/main.go
+++ cmd/main.go
@@ -25,11 +25,11 @@ var (
 )
 
 type appConfig struct {
-	CaptureInterval time.Duration `env:"CAPTURE_INTERVAL" envDefault:"100ms"`
+	CaptureInterval time.Duration `env:"CAPTURE_INTERVAL" envDefault:"80ms"`
 	ColorAlgo       string        `env:"COLOR_ALGO" envDefault:"AVERAGE"`
 	LightType       string        `env:"LIGHT_TYPE" envDefault:"LIFX"`
 	LightGroupName  string        `env:"LIGHT_GROUP_NAME" envDefault:"ARCADE"`
-	MaxBrightness   float64       `env:"MAX_BRIGHTNESS" envDefault:"0.7"`
+	MaxBrightness   float64       `env:"MAX_BRIGHTNESS" envDefault:"0.65"`
 	MinBrightness   float64       `env:"MIN_BRIGHTNESS" envDefault:"0"`
 	PixelGridSize   int           `env:"PIXEL_GRID_SIZE" envDefault:"5"`
 	ScreenNumber    int           `env:"SCREEN_NUMBER" envDefault:"0"`
@@ -45,11 +45,14 @@ func main() {
 
 	logger.With(zap.Any("config", config)).Info("Starting arcade lights")
 
-	logger.Info("Adjust SCREEN_NUMBER to target a different screen. 0 is the primary screen.")
-	logger.Info("Adjust PIXEL_GRID_SIZE to increase performance or accuracy. Lower values are slower but more accurate. 1 being the most accurate.")
+	logger.Info("Adjust CAPTURE_INTERVAL to change how often the screen is captured. (should generally be greater than 50ms due to screen capture latency)")
 	logger.Info("Adjust COLOR_ALGO to change color algorithm. Valid values are: [AVERAGE, SQUARED_AVERAGE, MEDIAN, MODE]")
+	logger.Info("Adjust PIXEL_GRID_SIZE to increase performance or accuracy. Lower values are slower but more accurate. 1 being the most accurate.")
 	logger.Info("LIGHT_TYPE only supports LIFX at the moment.")
 	logger.Info("Adjust LIGHT_GROUP_NAME to change the group of lights to control.")
+	logger.Info("Adjust MIN_BRIGHTNESS between 0 and 1.")
+	logger.Info("Adjust MAX_BRIGHTNESS between 0 and 1.")
+	logger.Info("Adjust SCREEN_NUMBER to target a different screen. 0 is the primary screen.")
 	logger.Info("Press Ctrl+C to stop")
 
 	ctx, cancel := context.WithCancel(context.Background())
